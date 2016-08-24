493794d51107eefb67931e8728786eaf63e833bc
jordanomac@gmail.com <jordanomac@gmail.com>
2016-08-24 12:01:31 -0600

    add dummy functions for settings page

--- app/pages/settings/settings.js
+++ app/pages/settings/settings.js
@@ -1,4 +1,4 @@
-import {Page, NavController} from 'ionic-angular';
+import {Page, Platform, NavController} from 'ionic-angular';
 
 @Page({
   templateUrl: 'build/pages/settings/settings.html',
@@ -11,5 +11,20 @@ export class SettingsPage {
   constructor(nav) {
     this.nav = nav;
   }
+    shareFB(message, subject, file, link) {
+        console.log('facebook share function fired');
+    }
+    shareTwitter(message, subject, file, link) {
+        console.log('twitter share function fired');
+    }
+    sendFrecklePhoto(image) {
+        console.log('send freckle photo function fired');
+    }
+    reportBug(){
+        console.log('report bug function fired');
+    }
+    termsPrivacy(){
+        console.log('terms of privacy function fired');
+    }
 
 }
