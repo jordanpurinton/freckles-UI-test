ded05ee9ab9e1cd9df0eeac7ef50031994ade37e
jordanomac@gmail.com <jordanomac@gmail.com>
2016-09-05 13:56:12 -0600

    change camera destination type, hopefully this will save nowg

--- app/app.js
+++ app/app.js
@@ -57,14 +57,16 @@ export class DefaultPage {
         console.log('click');
         Camera.getPicture({
             cameraDirection: Camera.Direction.FRONT,
-            destinationType: Camera.DestinationType.DATA_URL,
+            destinationType: Camera.DestinationType.FILE_URI,
+            correctOrientation: true,
+            saveToPhotoAlbum: true,
             targetWidth: 1000,
             targetHeight: 1000
 
         }).then((imageData) => {
             // imageData is a base64 encoded string
             this.base64Image = "data:image/jpeg;base64," + imageData;
-            //this.saveToPhotoAlbum(imageData);
+            this.saveToPhotoAlbum(imageData);
         }, (err) => {
             console.log(err);
         });
